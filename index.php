c685c7da9214e24e8492828c7079a883a2ff89a8
Gabriel Kanev <contact@mrgkanev.eu>
2024-08-30 21:41:51 +0300

    wip

--- index.php
+++ index.php
@@ -29,8 +29,134 @@ function checkWordPressSite($url)
     'hosting_provider' => null,
     'robots_txt' => null
   ];
+
+  // Normalize URL
+  $url = rtrim($url, '/');
+
+  // Check SSL
+  $results['ssl_enabled'] = (parse_url($url, PHP_URL_SCHEME) === 'https');
+  sendUpdate(['ssl_enabled' => $results['ssl_enabled']]);
+
+  // Initialize cURL session
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($ch, CURLOPT_USERAGENT, 'WordPress Site Checker Bot');
+  curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+
+  // Execute the request
+  $html = curl_exec($ch);
+
+  if (curl_errno($ch)) {
+    $results['issues'][] = "Error accessing the site: " . curl_error($ch);
+    sendUpdate(['issues' => $results['issues']]);
+    curl_close($ch);
+    return;
+  }
+
+  $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  if ($status_code != 200) {
+    $results['issues'][] = "Site returned status code: $status_code";
+    sendUpdate(['issues' => $results['issues']]);
+  }
+
+  // Get hosting provider information
+  $ip = gethostbyname(parse_url($url, PHP_URL_HOST));
+  $hosting_info = getHostingProvider($ip);
+  $results['hosting_provider'] = $hosting_info;
+  sendUpdate(['hosting_provider' => $results['hosting_provider']]);
+
+  curl_close($ch);
+
+  // Check robots.txt
+  $robots_txt = @file_get_contents("$url/robots.txt");
+  if ($robots_txt !== false) {
+    $results['robots_txt'] = "Found";
+    sendUpdate(['robots_txt' => $results['robots_txt']]);
+  } else {
+    $results['robots_txt'] = "Not found";
+    sendUpdate(['robots_txt' => $results['robots_txt']]);
+  }
+
+  // Check if it's a WordPress site
+  if (strpos($html, '/wp-content/') !== false) {
+    $results['is_wordpress'] = true;
+    sendUpdate(['is_wordpress' => true]);
+
+    // WordPress version check
+    preg_match('/<meta name="generator" content="WordPress ([0-9.]+)"/', $html, $version_matches);
+    if (!empty($version_matches[1])) {
+      $results['version'] = $version_matches[1];
+      $results['issues'][] = "WordPress version is visible: " . $version_matches[1];
+    }
+    sendUpdate(['version' => $results['version']]);
+
+    // Theme check
+    preg_match('/wp-content\/themes\/([^\/]+)/', $html, $theme_matches);
+    if (!empty($theme_matches[1])) {
+      $results['theme'] = $theme_matches[1];
+    }
+    sendUpdate(['theme' => $results['theme']]);
+
+    // Plugin check
+    preg_match_all('/wp-content\/plugins\/([^\/]+)/', $html, $plugin_matches);
+    if (!empty($plugin_matches[1])) {
+      $plugins = array_values(array_unique($plugin_matches[1]));
+      foreach ($plugins as $plugin) {
+        $plugin_file = "$url/wp-content/plugins/$plugin/$plugin.php";
+        $plugin_data = @file_get_contents($plugin_file);
+        if ($plugin_data !== false) {
+          preg_match('/Version:\s*(.+)$/m', $plugin_data, $version_match);
+          $version = !empty($version_match[1]) ? trim($version_match[1]) : 'Unknown';
+          $results['plugins'][] = "$plugin|$version";
+        } else {
+          $results['plugins'][] = "$plugin|Unknown";
+        }
+      }
+    }
+    sendUpdate(['plugins' => $results['plugins']]);
+
+    // Directory indexing check
+    $upload_dir = @file_get_contents("$url/wp-content/uploads/");
+    $plugin_dir = @file_get_contents("$url/wp-content/plugins/");
+    if (strpos($upload_dir, 'Index of') !== false || strpos($plugin_dir, 'Index of') !== false) {
+      $results['directory_indexing'] = true;
+      $results['issues'][] = "Directory indexing is enabled";
+    }
+    sendUpdate(['directory_indexing' => $results['directory_indexing']]);
+
+    // wp-cron.php check
+    $wp_cron = @file_get_contents("$url/wp-cron.php");
+    if ($wp_cron !== false) {
+      $results['wp_cron_found'] = true;
+      $results['issues'][] = "wp-cron.php is accessible";
+    }
+    sendUpdate(['wp_cron_found' => $results['wp_cron_found']]);
+
+    // User enumeration check
+    $user_check = @file_get_contents("$url/?author=1");
+    if ($user_check !== false && strpos($user_check, 'author/') !== false) {
+      $results['user_enumeration'] = true;
+      $results['issues'][] = "User enumeration is possible";
+    }
+    sendUpdate(['user_enumeration' => $results['user_enumeration']]);
+
+    // XML-RPC check
+    $xmlrpc = @file_get_contents("$url/xmlrpc.php");
+    if ($xmlrpc !== false && strpos($xmlrpc, 'XML-RPC server accepts POST requests only.') !== false) {
+      $results['xml_rpc_enabled'] = true;
+      $results['issues'][] = "XML-RPC is enabled";
+    }
+    sendUpdate(['xml_rpc_enabled' => $results['xml_rpc_enabled']]);
+  } else {
+    $results['issues'][] = "This does not appear to be a WordPress site.";
+    sendUpdate(['is_wordpress' => false]);
+  }
+
+  sendUpdate(['issues' => $results['issues']]);
 }
 
+// Function to get hosting provider
 function getHostingProvider($ip)
 {
   $hosting_providers = [
